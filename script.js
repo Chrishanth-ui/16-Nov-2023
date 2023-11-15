a8a938d37b9c1bf86649a5d2c39491a88fc0c7ef
CODE_BREAKER <82430205+Chrishanth-ui@users.noreply.github.com>
2023-11-15 19:13:50 +0530

    Signed-off-by: CODE_BREAKER <82430205+Chrishanth-ui@users.noreply.github.com>

---
+++ script.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('animContainer'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://lottie.host/2885cd61-325c-4ac6-b049-33eca5602717/470rGKWrSc.json' // lottie file path
+  })
